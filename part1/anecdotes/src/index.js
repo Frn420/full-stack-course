3d88d0ff703a6a0e2a6e28fe6d928ef68261d667
Frn420 <55882047+Frn420@users.noreply.github.com>
2020-08-07 20:32:04 +0200

    Add files via upload

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(6).fill(0))
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+
+    return setVotes(copy)
+  }
+
+  const handleRandom = () => setSelected(Math.floor(Math.random() * 6))
+
+  const maxVotes = () => {
+    let index = 0
+    let max = 0
+
+    for (let i = 0; i < 6; i++){
+      if (votes[i] > max){
+        index = i
+        max = votes[i]
+      }
+    }
+
+    return index
+  }
+
+  let max = maxVotes()
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{props.anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <div>
+        <Button handleClick={handleVote} text="vote" />
+        <Button handleClick={handleRandom} text="next anecdote" />
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <div>{props.anecdotes[max]}</div>
+      <div>has {votes[max]} votes</div>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistic = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const positive = [good / all * 100, ' %']
+
+  if (all === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <Statistic text='good' value={good} />
+        <Statistic text='neutral' value={neutral} />
+        <Statistic text='bad' value={bad} />
+        <Statistic text='all' value={all} />
+        <Statistic text='average' value={(good - bad) / all} />
+        <Statistic text='positive' value={positive} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header text='give feedback' />
+      <Button
+        handleClick={increaseGood}
+        text='good'
+      />
+      <Button
+        handleClick={increaseNeutral}
+        text='neutral'
+      />
+      <Button
+        handleClick={increaseBad}
+        text='bad'
+      />
+      <Header text='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
